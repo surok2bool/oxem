b14844fd47075ce183d4368106bcaccc1e7a74b8
Sur Ok <surokbool@gmail.com>
2020-08-22 18:26:55 +0300

    Добавлено условие для запроса - Content-Type: application/json

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,16 +3,27 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ApiController extends AbstractController
 {
 
-    public function index()
+    public function index(Request $request)
     {
-        return $this->json([
+        $data = $request->getContent();
+
+        $response = new Response();
+        $response->setStatusCode(200);
+
+        $responseBody = [
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ApiController.php',
-        ]);
+            'error' => false
+        ];
+
+        $response->setContent(json_encode($responseBody));
+
+        return $response;
     }
 }
