bb4c115e068c335212288b162388024e7242d695
Sur Ok <surokbool@gmail.com>
2020-08-23 11:04:53 +0300

    rename controller

--- src/Controller/ApiProductController.php
+++ src/Controller/ApiProductController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class ApiController extends AbstractController
+class ApiProductController extends AbstractController
 {
     /**
      * @var ValidatorInterface $validator
