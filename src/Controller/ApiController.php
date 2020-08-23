35060d65a7522c4449a8de54ce2c2d83b68c9350
Sur Ok <surokbool@gmail.com>
2020-08-23 11:00:30 +0300

    Добавлен компонент валидации, создана логика создания продукта, изменен роут для создания продукта

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,27 +2,77 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ApiController extends AbstractController
 {
+    /**
+     * @var ValidatorInterface $validator
+     */
+    private $validator;
 
-    public function index(Request $request)
+
+    /**
+     * ApiController constructor.
+     * @param ValidatorInterface $validator
+     */
+    public function __construct(ValidatorInterface $validator)
     {
-        $data = $request->getContent();
+        $this->validator = $validator;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function createProduct(Request $request): Response
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $product = new Product();
+
+        /**
+         * Результирующий массив, в котором будем собирать все ошибки и результат
+         */
+        $result = [
+            'success' => false
+        ];
+
+        try {
+            $product->setProductData($data);
+        } catch (EntityNotFoundException $e) {
+            $result['errors'][] = $e->getMessage();
+        }
+
+        $errors = $this->validator->validate($product);
+
+        if ($errors->count() > 0 || !empty($result['errors'])) {
+            foreach ($errors as $error) {
+                $result['errors'][] = $error->getMessage();
+            }
+        } else {
+            $entityManager->persist($product);
+            $entityManager->flush();
+        }
+
+        if (!empty($product->getId())) {
+            $result['success'] = true;
+            $result['payload']['id'] = $product->getId();
+        } else {
+            $result['errors'][] = 'Failed to create product';
+        }
 
         $response = new Response();
         $response->setStatusCode(200);
 
-        $responseBody = [
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ApiController.php',
-            'error' => false
-        ];
-
-        $response->setContent(json_encode($responseBody));
+        $response->setContent(json_encode($result));
 
         return $response;
     }
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -5,7 +5,9 @@ namespace App\Entity;
 use App\Repository\ProductRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\EntityNotFoundException;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ProductRepository::class)
@@ -20,11 +22,21 @@ class Product
     private $id;
 
     /**
+     * @Assert\Length(
+     *      min = 3,
+     *      max = 200,
+     *      minMessage = "Name must be at least {{ limit }} characters",
+     *      maxMessage = "Name must be no more than {{ limit }} characters"
+     * )
      * @ORM\Column(type="string", length=200)
      */
     private $name;
 
     /**
+     * @Assert\Length(
+     *      max = 1000,
+     *      maxMessage = "Description must be no more than {{ limit }} characters"
+     * )
      * @ORM\Column(type="text", nullable=true)
      */
     private $description;
@@ -40,9 +52,9 @@ class Product
     private $price;
 
     /**
-     * @ORM\Column(type="json")
+     * @ORM\Column(type="integer")
      */
-    private $stock = [];
+    private $stock;
 
     /**
      * @ORM\ManyToMany(targetEntity=Category::class)
@@ -54,21 +66,34 @@ class Product
      */
     private $externalId;
 
+    /**
+     * Product constructor.
+     */
     public function __construct()
     {
         $this->categories = new ArrayCollection();
     }
 
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @return string|null
+     */
     public function getName(): ?string
     {
         return $this->name;
     }
 
+    /**
+     * @param string $name
+     * @return $this
+     */
     public function setName(string $name): self
     {
         $this->name = $name;
@@ -76,11 +101,18 @@ class Product
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getDescription(): ?string
     {
         return $this->description;
     }
 
+    /**
+     * @param string|null $description
+     * @return $this
+     */
     public function setDescription(?string $description): self
     {
         $this->description = $description;
@@ -88,23 +120,36 @@ class Product
         return $this;
     }
 
+    /**
+     * @return \DateTimeInterface|null
+     */
     public function getDateCreate(): ?\DateTimeInterface
     {
         return $this->dateCreate;
     }
 
-    public function setDateCreate(\DateTimeInterface $dateCreate): self
+    /**
+     * @return $this
+     */
+    public function setDateCreate(): self
     {
-        $this->dateCreate = $dateCreate;
+        $this->dateCreate = new \DateTime();
 
         return $this;
     }
 
+    /**
+     * @return float|null
+     */
     public function getPrice(): ?float
     {
         return $this->price;
     }
 
+    /**
+     * @param float $price
+     * @return $this
+     */
     public function setPrice(float $price): self
     {
         $this->price = $price;
@@ -112,12 +157,20 @@ class Product
         return $this;
     }
 
-    public function getStock(): ?array
+    /**
+     * @return int|null
+     */
+    public function getStock(): ?int
     {
         return $this->stock;
     }
 
-    public function setStock(array $stock): self
+    /**
+     * Пока что храним значение остатка в виде integer
+     * @param int $stock
+     * @return $this
+     */
+    public function setStock(int $stock): self
     {
         $this->stock = $stock;
 
@@ -132,6 +185,10 @@ class Product
         return $this->categories;
     }
 
+    /**
+     * @param Category $category
+     * @return $this
+     */
     public function addCategory(Category $category): self
     {
         if (!$this->categories->contains($category)) {
@@ -141,6 +198,10 @@ class Product
         return $this;
     }
 
+    /**
+     * @param Category $category
+     * @return $this
+     */
     public function removeCategory(Category $category): self
     {
         if ($this->categories->contains($category)) {
@@ -150,15 +211,69 @@ class Product
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getExternalId(): ?string
     {
         return $this->externalId;
     }
 
+    /**
+     * Используем тип строки, поскольку внешний id может быть не только числовым
+     *
+     * @param string|null $externalId
+     * @return $this
+     */
     public function setExternalId(?string $externalId): self
     {
         $this->externalId = $externalId;
 
         return $this;
+    }
+
+    /**
+     * На вход принимаем массив id категорий, проверяем, существует ли указанная категория
+     * @param integer[] $categoriesIds
+     */
+    public function setCategories(array $categoriesIds)
+    {
+//        throw new EntityNotFoundException('Category not found');
+    }
+
+    /**
+     * Логику установки полей продукта хотелось собарть в одном месте и не размазывать по разным участкам кода.
+     * Намеренно не использую тут сеттеры, а устанавливаю каждое свойства руками, в данном случае нахожу это более
+     * удобным, а сеттеры иметь надо, поскольку всегда могут пригодиться в другом месте
+     *
+     * @param array $fields
+     */
+    public function setProductData(array $fields): void
+    {
+        $this->name = $fields['name'] ?? '';
+        $this->description = $fields['description'] ?? '';
+
+        $this->externalId = $fields['externalId'] ?? '';
+
+        /**
+         * Для цены используется тип float, для большего удобства пользования предположим, что
+         * пользователь может передать цену как строку, поэтому сначала попытаемся привести значение
+         * к float, в случае неудачи - проставим цену как 0.
+         */
+        if (!empty($fields['price']) && settype($fields['price'], 'float')) {
+            $this->price = round($fields['price'], 2);
+        } else {
+            $this->price = 0.00;
+        }
+
+        $this->stock = (!empty($fields['stock']) && settype($fields['stock'], 'integer'))
+            ? $fields['stock']
+            : 0;
+
+        if (!empty($fields['categories']) && is_array($fields['categories'])) {
+            $this->setCategories($fields['categories']);
+        }
+
+        $this->setDateCreate();
     }
 }
